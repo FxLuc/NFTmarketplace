b8e434109435c27a69afc0caba5ecbee0139b6b5
Flex Alderson <phantoinang2001@gmail.com>
2022-01-09 14:50:46 +0700

    fix mogoose, add handle error

--- server/server.js
+++ server/server.js
@@ -1,21 +1,16 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const PORT = 4000;
 const cors = require("cors");
-const mongoose = require("mongoose");
 const config = require("./config/db");
 const productsRoute = require("./routes/product.route");
 
-mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, { useNewUrlParser: true }).then(
-  () => {
-    console.log("Database is connected");
-  },
-  (err) => {
-    console.log("Can not connect to the database" + err);
-  }
-);
+// mongoose connect
+const mongoose = require('mongoose')
+mongoose.connect(config.DB)
+  .then(() => console.log('mongodb connected'))
+  .catch(error => handleError(error))
+
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,6 +18,23 @@ app.use(bodyParser.json());
 
 app.use("/products", productsRoute);
 
-app.listen(PORT, function () {
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.status(200).json({ message: '404 Not found' })
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
   console.log("Server is running on Port:", PORT);
 });
