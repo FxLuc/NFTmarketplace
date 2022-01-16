d4b4c53436e2dc3355bb6d7f22b74a832137116d
Flex Alderson <phantoinang2001@gmail.com>
2022-01-16 21:32:21 +0700

    hotfix update product

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,5 +2,5 @@ module.exports = app => {
     const { productController } = require('../controllers')
     app.get('/product', productController.getProduct)
 	app.post('/product/create', productController.createProduct)
-	app.post('/product/create', productController.updateProduct)
+	app.post('/product/update', productController.updateProduct)
 }
