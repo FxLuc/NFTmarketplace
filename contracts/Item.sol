8787270df3c1ee17f8a1be429394bc33e37aebef
Flex Alderson <phantoinang2001@gmail.com>
2022-02-05 11:28:15 +0700

    trigger payment

--- contracts/Item.sol
+++ contracts/Item.sol
@@ -1,29 +1,48 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.11;
 
+import "./Context.sol";
 import "./ItemManager.sol";
 
-contract Item {
-    uint public priceInWei;
-    uint public pricePaid;
-    uint public index;
-    ItemManager parentContract;
+contract Item is Context{
+    address public owner;
+    ItemManager public parentContract;
+    bytes32 public identifier;
+    bytes32 public specifications;
+    bytes32 public rawDataUrl;
+    bytes32 public rawDataHash;
+    uint public indexInParentContract;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
     
-    constructor(ItemManager _parentContract, uint _priceInWei, uint _indedx) public {
-        priceInWei = _priceInWei;
-        index = _indedx;
+    constructor(ItemManager _parentContract, bytes32 _identifier, bytes32 _specifications, bytes32 _rawDataUrl, bytes32 _rawDataHash, uint _indexInParentContract) {
+        owner = tx.origin;
+        identifier = _identifier;
+        specifications = _specifications;
+        rawDataUrl = _rawDataUrl;
+        rawDataHash = _rawDataHash;
+        indexInParentContract = _indexInParentContract;
         parentContract = _parentContract;
+    }
+
+    function transferOwnership(address _newOwner) public {
+        require(owner == msg.sender, "Item: caller is not the Item owner");
+        require(_newOwner != address(0), "Item: new owner is the zero address");
+        _transferOwnership(_newOwner);
+    }
+
+    function _transferOwnership(address _newOwner) internal {
+        address oldOwner = owner;
+        owner = _newOwner;
+        emit OwnershipTransferred(oldOwner, _newOwner);
     }
     
     receive() external payable {
-        require(pricePaid ==0, "Item is paid already!");
-        require(priceInWei == msg.value, "Only full payments allowed");
-        pricePaid+= msg.value;
-        (bool success,) = address(parentContract).call{value: msg.value}(abi.encodeWithSignature("triggerPayment(uint256)",index));
-        require(success,"The transaction wasn't successfull, cancelling");
+        // (bool success, ) = address(parentContract).call{value: msg.value}(abi.encodeWithSignature("triggerPayment(uint256)", indexInParentContract));
+        // require(success, "Item: the transaction wasn't successfull, cancelling...");
+        parentContract.triggerPayment{value: msg.value}(indexInParentContract);
+        _transferOwnership(_msgSender());
     }
     
-    fallback() external {
-        
-    }
+    fallback() external {}
 }
--- contracts/ItemManager.sol
+++ contracts/ItemManager.sol
@@ -1,48 +1,60 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.11;
 
 import "./Ownable.sol";
 import "./Item.sol";
 
 contract ItemManager is Ownable {
 
-    enum SupplyChainState{Created, Paid, Delivered}
-    
+    enum SupplyChainState{Added, Sale, Sold}
     struct S_Item {
         Item _item;
-        string _identifier;
-        uint _itemPrice;
+        uint _price;
         ItemManager.SupplyChainState _state;
     }
-    
     mapping (uint => S_Item) public items;
-    uint itemIndex;
-    event SupplyChainStep(uint _itemIndex, uint _step, address _itemAddress);
+    uint public itemIndex;
 
-    function getItemIndex() public onlyOwner view returns(uint) {
-        return itemIndex;
+    event SupplyChainStep(uint itemIndex, address indexed item, uint8 step);
+
+    modifier itemFound(uint _itemIndex) {
+        require(address(items[_itemIndex]._item) != address(0), "ItemManager: item not found!");
+        _;
+    }
+
+    modifier onlyItemOwner(uint _itemIndex) {
+        require(items[_itemIndex]._item.owner() == msg.sender, "ItemManager: caller is not the Item owner");
+        _;
     }
     
-    function createItem(string memory _identifier, uint _itemPrice) public onlyOwner {
-        Item item = new Item(this, _itemPrice, itemIndex);
-        items[itemIndex]._item = item;
-        items[itemIndex]._identifier = _identifier;
-        items[itemIndex]._itemPrice = _itemPrice;
-        items[itemIndex]._state = SupplyChainState.Created;
-        emit SupplyChainStep(itemIndex, uint(items[itemIndex]._state), address(item));
+    function createItem(bytes32 _identifier, bytes32 _specifications,  bytes32 _rawDataUrl, bytes32 _rawDataHash) public {
+        Item item = new Item(this, _identifier, _specifications, _rawDataUrl, _rawDataHash, itemIndex);
+        S_Item storage s_item = items[itemIndex];
+        s_item._item = item;
+        s_item._state = SupplyChainState.Added;
+        emit SupplyChainStep(itemIndex, address(item), uint8(s_item._state));
         itemIndex++;
     }
-    
-    function triggerPayment(uint _itemIndex) public payable {
-        require(items[_itemIndex]._itemPrice == msg.value, "Only full payments accepted!");
-        require(items[_itemIndex]._state == SupplyChainState.Created, "Item is further in the chain!");
-        items[_itemIndex]._state = SupplyChainState.Paid;
-        emit SupplyChainStep(_itemIndex, uint(items[_itemIndex]._state), address(items[_itemIndex]._item));
+
+    function triggerSale(uint _itemIndex, uint _price) public itemFound(_itemIndex) onlyItemOwner(_itemIndex){
+        S_Item storage s_item = items[_itemIndex];
+        require(s_item._state == SupplyChainState.Added, "ItemManager: This item state must be \"Added\" to make for sale!");
+        s_item._price = _price;
+        s_item._state = SupplyChainState.Sale;
+        emit SupplyChainStep(_itemIndex, address(s_item._item), uint8(s_item._state));
     }
     
-    function triggerDelivery(uint _itemIndex) public onlyOwner {
-        require(items[_itemIndex]._state == SupplyChainState.Paid, "Item is further in the chain!");
-        items[_itemIndex]._state= SupplyChainState.Delivered;
-        emit SupplyChainStep(_itemIndex, uint(items[_itemIndex]._state), address(items[_itemIndex]._item));
+    function triggerPayment(uint _itemIndex) public payable itemFound(_itemIndex) {
+        S_Item memory s_item = items[_itemIndex];
+        require(msg.sender == address(s_item._item),  "ItemManager: this function must be call from Item contract");
+        require(s_item._state == SupplyChainState.Sale, "ItemManager: this item is not for sale!");
+        require(s_item._price == msg.value, "ItemManager: only full payments accepted!");
+        items[_itemIndex]._state = SupplyChainState.Sold;
+        emit SupplyChainStep(_itemIndex, address(s_item._item), uint8(items[_itemIndex]._state));
+    }
+    
+    function triggerDelivery(uint _itemIndex) public onlyOwner itemFound(_itemIndex){
+        require(items[_itemIndex]._state == SupplyChainState.Sold, "ItemManager: Item is further in the chain!");
+        emit SupplyChainStep(_itemIndex, address(items[_itemIndex]._item), uint8(items[_itemIndex]._state));
     }
 }
