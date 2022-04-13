070d4063df4ec4af93df2642f83d780f028e4bed
Flex Alderson <phantoinang2001@gmail.com>
2022-04-13 16:15:59 +0700

    split order controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,3 +1,4 @@
 module.exports.itemController = require('./item.controller')
+module.exports.orderController = require('./order.controller')
 module.exports.accountController = require('./account.controller')
 module.exports.infuraController = require('./infura.controller')
